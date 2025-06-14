e2fe7cb21b9dec59984937bfb8a630d9663983b7
Alex K. <kapul@list.ru>
2025-06-14 16:18:33 +0300

    Рабочая версия.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         applicationId = "com.epc_it.fuelstations"
         minSdk = 24
         targetSdk = 35
-        versionCode = 4
+        versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
